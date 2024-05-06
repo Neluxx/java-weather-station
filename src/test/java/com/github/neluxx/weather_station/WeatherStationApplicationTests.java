4c3cbcfb3d6579b8bb39cacf126a5b9d0cc9e71e
Fabian Arndt <fabian.arndt96@proton.me>
2024-05-06 19:08:00 +0200

    Setup java spring boot

---
+++ src/test/java/com/github/neluxx/weather_station/WeatherStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.neluxx.weather_station;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
